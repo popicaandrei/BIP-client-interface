6cc20364fbda521da3ab2395c625186b524ebbb6
Andrei Popica <popica.andreivlad@gmail.com>
2022-05-27 18:01:34 +0300

    Navbar component created

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,17 +6,23 @@ export default function Navbar({user}) {
     return (
         <header className="header-section">
             <nav className="nav">
+                <div className="nav-avatar">
+                    <div className="inside-avatar-icon">
+                        <Avatar text={user.name} color="gradient" textColor="white" size="lg"/>
+                    </div>
+                    <div className="inside-avatar">
+                        <Text h4 weight="bold"
+                              css={{textGradient: "45deg, $blue600 -20%, $pink600 50%",}}>Hello, {user.name} </Text>
+                    </div>
+                </div>
                 <div className="nav-title">
                     <Text h3 css={{textGradient: "45deg, $yellow700 -20%, $red600 100%",}}
                           weight="bold">{user.institutionName} City Portal</Text>
                 </div>
 
                 <div className="nav-items">
-                    <Avatar text={user.name} color="gradient" textColor="white" size="lg"/>
                     <Text h4 weight="bold"
-                          css={{textGradient: "45deg, $blue600 -20%, $pink600 50%",}}>Welcome, {user.name} </Text>
-                    <Text h4 weight="bold"
-                              css={{textGradient: "45deg, $blue600 -20%, $pink600 50%",}}>Logout</Text>
+                          css={{textGradient: "45deg, $blue600 -20%, $pink600 50%",}}>Logout</Text>
                 </div>
             </nav>
         </header>
