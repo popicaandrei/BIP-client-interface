49f793be0fe402097b7138f44f4df5f9674a1506
Andrei Popica <popica.andreivlad@gmail.com>
2022-05-28 22:27:50 +0300

    Created event service for retrieving data from api

---
+++ client/src/services/EventService.js
@@ -0,0 +1,34 @@
+import {ApiUtil} from "../utils/ApiUtil";
+import axios from "axios";
+
+export async function getEventsForInstitution() {
+    let path = ApiUtil.URL + "/events";
+    try {
+        const token = localStorage.getItem("jwt");
+        if (token) {
+            let response = await axios.get(path, {
+                headers: {"Authorization": token},
+            });
+            return response.data;
+        }
+    } catch {
+        console.log("Error getting the events");
+    }
+}
+
+export async function addEvent(event) {
+    let path = ApiUtil.URL + "/login";
+    try {
+        await axios.post(path, {
+            email: event.name,
+            reward: event.reward,
+            active: event.active,
+            validationNedeed: event.validation,
+            authType: event.authType
+        }).then((response) => {
+            return response.status;
+        });
+    } catch {
+        console.log("Error adding event");
+    }
+}
